2103733b238c6ac5767e07e7daebea39cf0cbd65
Gustavo CM <gustavo.alocoma@gmail.com>
2020-12-28 00:12:57 -0500

    titutlo del forumulario producto

--- app1.js
+++ app1.js
@@ -1,35 +1,35 @@
-var express = require('express');
-var routes = require('./routes');
-var http = require('http');
-var path = require('path');
+var express = require('express')
+var routes = require('./routes')
+var http = require('http')
+var path = require('path')
 
 //
-var productos = require('./routes/productos'); 
-var app = express();
-var connection  = require('express-myconnection'); 
-var mysql = require('mysql');
+var productos = require('./routes/productos')
+var app = express()
+var connection = require('express-myconnection')
+var mysql = require('mysql')
 
 //all environments
-app.set('port', process.env.PORT || 4300);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
+app.set('port', process.env.PORT || 4300)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.favicon())
+app.use(express.logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.methodOverride())
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(express.errorHandler())
 }
 /*------------------------------------------
     connection peer, register as middleware
     type koneksi : single,pool and request 
 -------------------------------------------*/
-app.use(  
+/* app.use(  
     connection(mysql,{
         host: 'localhost',
         user: 'root',
@@ -37,8 +37,21 @@ app.use(
         port : 3306, //port mysql
         database:'examena'
     },'pool') //or single
-);
-app.get('/',routes.index);
+); */
+app.use(
+  connection(
+    mysql,
+    {
+      host: 'localhost',
+      user: 'jaszert',
+      password: 'admin',
+      port: 3306, //port mysql
+      database: 'ex_web',
+    },
+    'pool'
+  ) //or single
+)
+app.get('/', routes.index)
 /*app.get('/clientes/frmcliente',clientes.frmcliente);
 app.post('/clientes/save',clientes.save);
 app.post('/clientes/remove',clientes.remove);
@@ -48,8 +61,8 @@ app.post('/clientes/buscarxdni',clientes.buscarxdni);
 app.post('/clientes/validarcliente',clientes.validarcliente);
 app.post('/clientes/getTotalRegs',clientes.getTotalRegs);*/
 
-app.get('/productos/frmproducto',productos.frmproducto);
-app.post('/productos/save',productos.save);
+app.get('/productos/frmproducto', productos.frmproducto)
+app.post('/productos/save', productos.save)
 /*app.post('/productos/remove',producto.remove);
 app.post('/productos/update',producto.update);
 app.post('/clientes/buscar',producto.buscar);*/
@@ -58,8 +71,8 @@ app.post('/clientes/buscar',producto.buscar);*/
 //app.post('/productos/getTotalRegs',producto.getTotalRegs);
 /////
 
-app.use(app.router);
+app.use(app.router)
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server escuchando en puerto ' + app.get('port'));
-});
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server escuchando en puerto ' + app.get('port'))
+})
